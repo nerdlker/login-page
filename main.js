07d6116a24825fc2dec138564a8356d56a8a66b8
nerdlekar <nerdlekar@gmail.com>
2022-08-25 14:23:13 +0530

    added password match check

--- main.js
+++ main.js
@@ -1,3 +1,23 @@
+const password = document.querySelector("#password");
+const cpassword = document.querySelector("#cpassword");
+const submitButton = document.querySelectorAll(".create-account");
+const email = document.getElementById("email");
+
+submitButton.forEach((button) => {
+  button.addEventListener("click", (event) => {
+    console.log("i was pressed");
+    if (password.value === cpassword.value && password.value !== "") {
+      console.log("passwords Match");
+      password.classList.remove("passworderror");
+    } else {
+      console.log("passwords DO NOT match");
+      cpassword.classList.add("passworderror");
+      password.classList.add("passworderror");
+      event.preventDefault();
+    }
+  });
+});
+
 /*
 
 
